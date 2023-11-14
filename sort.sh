353590c66546e1da2fb49725a78980ad90115f5e
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-11-14 16:56:52 +0100

    Tasks Done

---
+++ sort.sh
@@ -0,0 +1,21 @@
+sort mix.txt
+
+sort inputfile.txt > filename.txt
+
+sort -o filename.txt inputfile.txt
+
+sort -r inputfile.txt
+
+sort -n filename.txt
+
+sort -nr filename.txt
+
+sort -k filename.txt
+
+sort -k 2n employee.txt
+
+sort -c cars.txt
+
+sort -u filename.txt
+
+sort -M filename.txt
