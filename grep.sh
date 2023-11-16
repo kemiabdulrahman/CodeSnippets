e5eda6b92b046536e412e2c006458db4d0987d5c
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-11-16 09:40:17 +0100

    Tasks Updated

--- grep.sh
+++ grep.sh
@@ -7,3 +7,16 @@
  grep -wi -B 4 "div" hello.txt
 
  grep -wi -A 4 "div" hello.txt
+
+grep "fruit" fruits.txt
+
+grep -i "berries" fruits.txt
+
+grep -v "berries" fruits.txt
+
+grep -n "berries" fruits.txt
+
+grep -w  "fruit" fruits.txt
+
+grep -c "fruit" fruits.txt
+
